d74ada32ccf65f15bdaa58ef6fe6b50a2c6bce7c
xJohonypksx <L202307014@vbravo.tecnm.mx>
2025-03-19 10:48:39 -0600

    CREACION DE LA VISTA DE JONA TU PA

---
+++ resources/views/test_git.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.app")
+@section("content")
+
+<h1 class="alert alert-success">JONATHAN GONZALEZ OSORIO</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('test_git', function () {
+    return view('test_git');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
