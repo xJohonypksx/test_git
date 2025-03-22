fc1ed2a79aa0e668605a3c53718e1644d07fde32
Kevin Cruz Cruz <L202307012@vbravo.tecnm.mx>
2025-03-22 10:07:34 -0600

    Modificacion agregue mi nombre Kevin Cruz Cruz y agregue una vista nueva

--- resources/views/Kevin.blade.php
+++ resources/views/Kevin.blade.php
@@ -1,7 +1,6 @@
 @extends("layouts.app")
 @section("content")
 
-<h1 class="alert alert-success">JONATHAN GONZALEZ OSORIO</h1>
-
 <h1 class="alert alert-info">KEVIN CRUZ CRUZ</h1>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('test_git', function () {
     return view('test_git');
 });
+
+Route::get('Kevin', function () {
+    return view('Kevin');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
