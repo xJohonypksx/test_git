0a1cc05bf39ac17df2e0dd53c83eafd51f109fb8
Perla-gard <L202307043@vbravo.tecnm.mx>
2025-03-22 19:12:34 -0600

    Vista Perla

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ Route::get('Kevin', function () {
     return view('Kevin');
 });
 
+Route::get('Kevin', function () {
+    return view('Perla');
+});
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
