4539c16c9f1a8d831574ca4daed3650c3f67d643
yairsoto777 <l202307068@vbravo.tecnm.mx>
2025-03-26 10:06:52 -0600

    cambie el centrado de la tabla

--- resources/views/ciudad/index.blade.php
+++ resources/views/ciudad/index.blade.php
@@ -1,33 +1,32 @@
 @extends("layouts.app")
 @section("content")
+<div class="row justify-content-center">
+    <div class = "col-md-7 text-center">
+            <h1 class="alert">ciudad</h1>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class = "col-md-7 text-center">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
 
-    <h1>Ciudades</h1>
+                <th scope="col">Nombre</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
 
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Id ciudad</th>
-            <th scope="col">Nombre</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td>1</td>
-            <td>Nombre Queretaro</td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>2</td>
-            <td>Nombre CDMX</td>
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td>3</td>
-            <td>Nombre Guanajuato</td>
-        </tr>
-        </tbody>
-    </table>
+                <td>Nombre Queretaro</td>
+            </tr>
+
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::get('misa', function () {
 
 Route :: resource('clasificacion', App\Http\Controllers\ClasificacionController::class);
 
-Route::resource('Horas', App\Http\Controllers\HorasController::class);
+Route::resource('horas', App\Http\Controllers\HorasController::class);
 
 Route::resource('Ciudad', App\Http\Controllers\CiudadController::class);
 
