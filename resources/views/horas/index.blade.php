eddd827a16b49385311e910fdc954dfe1e45f442
13gustavo13 <L202307010@VBRAVO.TECNM.MX>
2025-03-26 10:49:19 -0600

    cambios horas

--- resources/views/horas/index.blade.php
+++ resources/views/horas/index.blade.php
@@ -1,31 +1,40 @@
 @extends("layouts.app")
+
 @section("content")
-<div class="row justify-content-center">
-    <div class = "col-md-7 text-center">
-            <h1 class="alert">hora</h1>
+    <div class="row justify-content-center my-5">
+        <div class="col-md-7 text-center">
+            <h1 class="alert alert-primary fw-bold">HORAS</h1>
+        </div>
     </div>
-</div>
-<div class="row justify-content-center">
-    <div class = "col-md-7 text-center">
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
 
-                <th scope="col">id_hora</th>
-
-                <th scope="col">descripcion</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>1</td>
-                <td>descripcion hora</td>
-            </tr>
-
-            </tbody>
-        </table>
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+            <div class="card shadow-sm rounded">
+                <div class="card-header bg-primary text-white text-center">
+                    <h2 class="h5 mb-0">Listado de Horas</h2>
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped table-hover">
+                        <thead class="table-dark text-center">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">ID</th>
+                            <th scope="col">Descripción</th>
+                        </tr>
+                        </thead>
+                        <tbody class="text-center">
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>1</td>
+                            <td>Descripción Hora</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-center">
+                    <small class="text-muted">Actualizado recientemente</small>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 @endsection
