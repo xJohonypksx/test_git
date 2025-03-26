fef0af082e53c78cad03a08481d194c3ceab945d
Perla-gard <L202307043@vbravo.tecnm.mx>
2025-03-25 22:39:33 -0600

    Creacion de la tabla clasificaciones

---
+++ app/Http/Controllers/ClasificacionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Clasificacion;
+use Illuminate\Http\Request;
+
+class ClasificacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('clasificaciones.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return"HOLA DESDE CREATE";//
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+       return "HOLA DESDE STORE"; //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Clasificacion $clasificacion)
+    {
+        return "HOLA DESDE SHOW";//
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Clasificacion $clasificacion)
+    {
+       return "HOLA DESDE EDIT"; //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Clasificacion $clasificacion)
+    {
+       return "HOLA DESDE UPDATE"; //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Clasificacion $clasificacion)
+    {
+        return "HOLA DESDE DESTROY";//
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ Route::get('Kevin', function () {
     return view('Kevin');
 });
 
-Route::get('Kevin', function () {
+Route::get('Perla', function () {
     return view('Perla');
 });
 
-
+Route :: resource('clasificacion', App\Http\Controllers\ClasificacionController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
